a3a8dffd4f359d12d20418300d0d42ba18d18cb1
jason2020 <jtay20@punahou.edu>
2022-07-09 18:29:37 -0700

    Moved some text into p tags and code into span

--- src/components/pages/searching.tsx
+++ src/components/pages/searching.tsx
@@ -22,24 +22,49 @@ function Searching(): JSX.Element {
           <div id="helpful-commands">
             <div className="helpful-command-row">Helpful Commands</div>
             <div className="helpful-command-row">
-              <div>Find a file under a specified directory</div>
-              <div>find [directoryToSearch] -name [fileToFind]</div>
+              <div>
+                <p>Find a file under a specified directory</p>
+              </div>
+              <div>
+                <span>find [directoryToSearch] -name [fileToFind]</span>
+              </div>
             </div>
             <div className="helpful-command-row">
-              <div>Find a directory under a specified directory</div>
-              <div>find [directoryToSearch] -type d -name [fileToFind]</div>
+              <div>
+                <p> Find a directory under a specified directory</p>
+              </div>
+              <div>
+                <span>
+                  {' '}
+                  find [directoryToSearch] -type d -name [fileToFind]
+                </span>
+              </div>
             </div>
             <div className="helpful-command-row">
-              <div>Find all hidden files</div>
-              <div>find [directoryToSearch] -type f -name &quot;.* &quot;</div>
+              <div>
+                <p>Find all hidden files</p>
+              </div>
+              <div>
+                <span>
+                  find [directoryToSearch] -type f -name &quot;.* &quot;
+                </span>
+              </div>
             </div>
             <div className="helpful-command-row">
-              <div>Find a string in a file</div>
-              <div>grep &quot;[string]&quot; [filename]</div>
+              <div>
+                <p>Find a string in a file</p>
+              </div>
+              <div>
+                <span>grep &quot;[string]&quot; [filename]</span>
+              </div>
             </div>
             <div className="helpful-command-row">
-              <div>Find a string in all files of a directory</div>
-              <div>grep -R &quot;[string]&quot; [filename] [directory]</div>
+              <div>
+                <p>Find a string in all files of a directory</p>
+              </div>
+              <div>
+                <span>grep -R &quot;[string]&quot; [filename] [directory]</span>
+              </div>
             </div>
           </div>
         </div>
@@ -109,12 +134,12 @@ function Searching(): JSX.Element {
               </div>
             </div>
             <div className={showTask1 ? 'task-content' : 'hidden'}>
-              <div>
+              <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
-              </div>
+              </p>
               <div className="terminal">terminal</div>
               <div className="submit">submit</div>
             </div>
@@ -131,12 +156,12 @@ function Searching(): JSX.Element {
               </div>
             </div>
             <div className={showTask2 ? 'task-content' : 'hidden'}>
-              <div>
+              <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
-              </div>
+              </p>
               <div className="terminal">terminal</div>
               <div className="submit">submit</div>
             </div>

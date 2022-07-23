3a28cd8e8f75091db4f5ae1b44c2b33d1217cb1c
snigdhakansal <snigdha0206@g.ucla.edu>
2022-07-23 13:44:52 -0700

    applied styling tags to creation.tsx

--- src/components/pages/creation.tsx
+++ src/components/pages/creation.tsx
@@ -1,99 +1,123 @@
-function Creation(): JSX.Element {
+import '../../styles/PageStyling.scss';
+
+function Creation1(): JSX.Element {
   return (
-    <div style={{ textAlign: 'left', paddingLeft: '2.5vw' }}>
-      Creation and Deletion
+    <div>
+      <h1 className="lesson-title">Creation and Deletion</h1>
+      <p className="body">
+        At the heart of any project is the ability to create and delete things.
+        So let’s start with making commands.
+      </p>
       <br />
-      <br />
-      At the heart of any project is the ability to create and delete things. So
-      let’s start with making commands.
-      <br />
-      <br />
-      touch
-      <br />
-      The touch command makes a new empty file.
-      <br />
-      Run `touch emptyFile` and see how the file system diagram changes.
-      <br />
-      If you then run `cat emptyFile`, notice how nothing is printed because the
-      file has no contents!
-      <br />
-      <br />
-      mkdir
-      <br />
-      The mkdir command makes a new empty directory.
-      <br />
-      Run `mkdir newDir` and see how the file system diagram changes.
-      <br />
-      Notice how you can now change into the newly-created directory with `cd
-      newDir`!
-      <br />
-      Removing commands are quite similar (plus, the names basically tell you
-      what the command does!).
-      <br />
-      <br />
-      rm
-      <br />
-      The rm command removes an existing file.
-      <br />
-      Looks like we have three files in the current directory.
-      <br />
-      Go ahead and remove one of them with the command `rm fileName`.
-      <br />
-      Can rm be used to remove a directory? Give it a try.
-      <br />
-      Notice how the file system diagram remains unchanged because rm cannot be
-      used to remove directories.
-      <br />
-      The terminal also printed an error message: rm: DIRNAME: is a directory
-      <br />
-      <br />
-      rmdir
-      <br />
-      The rmdir command removes an existing empty directory.
-      <br />
-      Try running `rmdir nonemptyDir`.
-      <br />
-      Notice how the terminal prints the error message: rmdir: nonemptyDir:
-      Directory not empty
-      <br />
-      We could manually rm all the files in a directory one by one, but there’s
-      an easier way: the -rf options combination.
-      <br />
-      For example,
-      <br />
-      `rm -rf nonemptyDir` recursively and forcefully (i.e., does not ask for
-      confirmation)
-      <br />
-      removes all files within the nonemptyDir directory.
-      <br />
-      <br />
-      cp
-      <br />
-      The cp command copies one file to another.
-      <br />
-      `cp sourceFile destinationFile` copies the contents of sourceFile into
-      destinationFile,
-      <br />
-      though the two will have different timestamps.
-      <br />
-      `cp file1 file2 directoryPath` copies the contents of file1 and file2 to
-      two new files in the directoryPath directory,
-      <br />
-      though the two new files will have different timestamps from the two
-      original files.
-      <br />
-      <br />
-      mv
-      <br />
-      The mv command moves/renames files.
-      <br />
-      `mv oldName newName` renames a file called oldName to newName, with the
-      timestamp remaining unchanged.
-      <br />
-      `mv file1 file2 directoryPath` moves file1 and file2 to directoryPath,
-      with the timestamps remaining unchanged.
+      <h2 className="heading-1">
+        The <span className="command">touch</span> Command
+      </h2>
+      <p className="body">
+        The <span className="try-out-command">touch</span> command makes a new
+        empty file.
+        <br />
+        Run <span className="try-out-command">touch emptyFile</span> and see how
+        the file system diagram changes.
+        <br />
+        If you then run <span className="try-out-command">cat emptyFile</span>,
+        notice how nothing is printed because the file has no contents!
+      </p>
+      <h2 className="heading-1">
+        The <span className="command">mkdir</span> Command
+      </h2>
+      <p className="body">
+        <span className="try-out-command">The mkdir</span> command makes a new
+        empty directory.
+        <br />
+        Run <span className="try-out-command">mkdir newDir</span> and see how
+        the file system diagram changes.
+        <br />
+        Notice how you can now change into the newly-created directory with{' '}
+        <span className="try-out-command">cd newDir</span>!
+      </p>
+      <p className="body">
+        Removing commands are quite similar (plus, the names basically tell you
+        what the command does!).
+      </p>
+      <h2 className="heading-1">
+        The <span className="command">rm</span> Command
+      </h2>
+      <p className="body">
+        The <span className="try-out-command">rm</span> command removes an
+        existing file.
+        <br />
+        Looks like we have three files in the current directory.
+        <br />
+        Go ahead and remove one of them with the command{' '}
+        <span className="try-out-command">rm fileName</span>.
+        <br />
+        Can rm be used to remove a directory? Give it a try.
+        <br />
+        Notice how the file system diagram remains unchanged because rm cannot
+        be used to remove directories.
+        <br />
+        The terminal also printed an error message: rm: DIRNAME: is a directory
+      </p>
+      <h2 className="heading-1">
+        The <span className="command">rmdir</span> Command
+      </h2>
+      <p className="body">
+        The rmdir command removes an existing empty directory.
+        <br />
+        Try running <span className="try-out-command">rmdir nonemptyDir</span>.
+        <br />
+        Notice how the terminal prints the error message: rmdir: nonemptyDir:
+        Directory not empty
+        <br />
+        We could manually rm all the files in a directory one by one, but
+        there’s an easier way: the -rf options combination.
+        <br />
+        For example,
+        <br />
+        <span className="try-out-command">rm -rf nonemptyDir</span> recursively
+        and forcefully (i.e., does not ask for confirmation)
+        <br />
+        removes all files within the nonemptyDir directory.
+      </p>
+      <h2 className="heading-1">
+        The <span className="command">cp</span> Command
+      </h2>
+      <p className="body">
+        The cp command copies one file to another.
+        <br />
+        <span className="try-out-command">
+          cp sourceFile destinationFile
+        </span>{' '}
+        copies the contents of sourceFile into destinationFile,
+        <br />
+        though the two will have different timestamps.
+        <br />
+        <span className="try-out-command">
+          cp file1 file2 directoryPath
+        </span>{' '}
+        copies the contents of file1 and file2 to two new files in the
+        directoryPath directory,
+        <br />
+        though the two new files will have different timestamps from the two
+        original files.
+      </p>
+      <h2 className="heading-1">
+        The <span className="command">mv</span> Command
+      </h2>
+      <p className="body">
+        The mv command moves/renames files.
+        <br />
+        <span className="try-out-command">mv oldName newName</span> renames a
+        file called oldName to newName, with the timestamp remaining unchanged.
+        <br />
+        <span className="try-out-command">
+          mv file1 file2 directoryPath
+        </span>{' '}
+        moves file1 and file2 to directoryPath, with the timestamps remaining
+        unchanged.
+      </p>
     </div>
   );
 }
 
-export default Creation;
+export default Creation1;

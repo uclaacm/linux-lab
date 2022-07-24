6fe0d8ffd717d8692ba91995a5652091a2c9869e
Lily Zhou <lzhou0714@g.ucla.edu>
2022-07-24 14:44:58 -0700

    formatted intro page

--- src/components/pages/intro.tsx
+++ src/components/pages/intro.tsx
@@ -1,5 +1,29 @@
+import '../../styles/intro.scss';
+
 function Intro(): JSX.Element {
-  return <div>intro</div>;
+  return <div className="intro-container">
+    <h1>Lesson Title</h1>
+    <p className = "intro-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+    </p>
+    <h2>Subsection</h2>
+    <p className="intro-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+    </p>
+    <div className='image-box'>
+        image here
+    </div>
+  </div>;
 }
 
 export default Intro;

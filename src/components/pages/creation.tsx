495def2591caa01ad995c65cb155a2c2d0da5222
snigdhakansal <snigdha0206@g.ucla.edu>
2022-07-24 01:13:36 -0700

    added props for tbe progressbar function

--- src/components/pages/creation.tsx
+++ src/components/pages/creation.tsx
@@ -1,5 +1,12 @@
+import Bar from '../shared/progressbar';
+
 function Creation(): JSX.Element {
-  return <div>Creation</div>;
+  return (
+    <div>
+      Creation
+      <Bar totalsteps={6} currentstep={2} />
+    </div>
+  );
 }
 
 export default Creation;
--- src/components/shared/progressbar.tsx
+++ src/components/shared/progressbar.tsx
@@ -1,19 +1,51 @@
-import './style.css';
+import './progressbarstyle.css';
 
-function Bar(): JSX.Element {
+interface Props {
+  totalsteps: number;
+  currentstep: number;
+}
+const Bar: React.FC<Props> = ({ totalsteps, currentstep }: Props) => {
+  const arr: number[] = [];
+  for (let i = 1; i <= totalsteps; i++) {
+    arr.push(i);
+  }
   return (
     <div>
       <div id="progress">
         <div id="progress-bar"></div>
-        <ul id="progress-num">
-          <li className="step active">1</li>
-          <li className="step">2</li>
-          <li className="step">3</li>
-          <li className="step">4</li>
-        </ul>
+        <div id="progress-num">
+          <div
+            id="progress-num2"
+            style={{
+              width: `calc(100% * ${(currentstep - 1) / (totalsteps - 1)})`,
+            }}
+          ></div>
+          {arr.map((title) => {
+            if (title < currentstep) return <li className="step active">{}</li>;
+            else return <li className="step">{}</li>;
+          })}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Bar;
+
+/*MIT License
+  Copyright (c) 2021 w3collective
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.*/

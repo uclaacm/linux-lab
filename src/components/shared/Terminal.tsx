a5b4e1f0f55e59fef8dc233d13472f32d732ea29
Arush Ramteke <arushramtek@gmail.com>
2022-08-03 10:26:42 +0800

    sike

--- src/components/shared/Terminal.tsx
+++ src/components/shared/Terminal.tsx
@@ -2,35 +2,40 @@ import React, { useState } from 'react';
 import '../../styles/Terminal.scss';
 
 function Terminal(): JSX.Element {
-    const [commands, setCommands] = useState<string[]>([]);
-    const [input, setInput] = useState('$ ');
+  const [commands, setCommands] = useState<string[]>([]);
+  const [input, setInput] = useState('$ ');
 
-    const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-        setInput(e.currentTarget.value);
-    };
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setInput(e.currentTarget.value);
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        setCommands([...commands, input]);
-        setInput('$ ');
-    };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setCommands([...commands, input]);
+    setInput('$ ');
+  };
 
-    return (
-        <div className="terminal">
-            <div>
-                {commands.map((command: string, key:number) => {
-                    return <p className="command" key={key}>{command}</p>;
-                })}
-            </div>
-            <form onSubmit={handleSubmit} id="input-form">
-                <input type="text"
-                    className="command"
-                    value={input}
-                    onChange={handleChange}
-                />
-            </form>
-        </div>
-    );
-  }
+  return (
+    <div className="terminal">
+      <div>
+        {commands.map((command: string, key: number) => {
+          return (
+            <p className="command" key={key}>
+              {command}
+            </p>
+          );
+        })}
+      </div>
+      <form onSubmit={handleSubmit} id="input-form">
+        <input
+          type="text"
+          className="command"
+          value={input}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+}
 
-  export default Terminal;
+export default Terminal;

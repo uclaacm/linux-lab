090ead7fed88c736ac39cec2b69a60490b4114d6
snigdhakansal <snigdha0206@g.ucla.edu>
2022-08-05 13:42:43 -0700

    adds snowfall to landing page

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -1,9 +1,17 @@
 import '../../styles/startButton.scss';
+import Snowfall from 'react-snowfall';
 import img from '../../assets/landing.svg';
 
 function Landing(): JSX.Element {
   return (
     <div>
+      <Snowfall
+        color="white"
+        snowflakeCount={200}
+        radius={[1.0, 6.0]}
+        speed={[1.0, 5.0]}
+        wind={[-0.5, 2.5]}
+      />
       <img style={{ width: '100vw' }} src={img}></img>
       <a href="intro">
         <button className="startbutton">START</button>

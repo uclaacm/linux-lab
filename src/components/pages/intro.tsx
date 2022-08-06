25d284c6a0ce7ae1c513a6d0c138c9e88c0abec4
Lily Zhou <lzhou0714@g.ucla.edu>
2022-08-06 12:22:22 -0700

    added text from hi-fi

--- src/components/pages/intro.tsx
+++ src/components/pages/intro.tsx
@@ -1,27 +1,27 @@
 import '../../styles/intro.scss';
-import posixFileSystem from "../../assets/images/posix-file-system.png";
+import posixFileSystem from '../../assets/images/posix-file-system.png';
 // import "../../styles/global.scss"
 function Intro(): JSX.Element {
   return (
     <div className="intro-container">
       <div className='lesson-title'>INTRO TO LINUX</div>
       <p className="body">
-      Linux is a family of operating systems. An operating system is a piece of software 
-      that acts as an interface between the user and the computer’s resources by providing 
+      Linux is a family of operating systems. An operating system is a piece of software
+      that acts as an interface between the user and the computer’s resources by providing
       resources for applications to build off of.
       </p>
       <p className="body">
-      Linux is one of the most widely used operating systems today in part because it is a FOSS 
+      Linux is one of the most widely used operating systems today in part because it is a FOSS
       or free or open source software. Anyone can obtain a copy of the Linux kernel and build
       their own Linux operating system! Understanding Linux is helpful to know because it helps
-       build and run many different kinds of applications today. Whether it's hacking into a 
-       computer or helping to put someone on the moon, Linux can help perform all of these various tasks! 
+       build and run many different kinds of applications today. Whether it's hacking into a
+       computer or helping to put someone on the moon, Linux can help perform all of these various tasks!
       </p>
       <p className="body">
-      In this learning lab, you will be learning how to speak the language of Linux or 
-      learning basic commands that often apply through a command-line interface (CLI). 
+      In this learning lab, you will be learning how to speak the language of Linux or
+      learning basic commands that often apply through a command-line interface (CLI).
       These commands will not only teach you how to move about the Linux file system but
-       also teach you how to command the computer to do what you want! 
+       also teach you how to command the computer to do what you want!
       </p>
 
       <div className='heading-1'>THE POSIX FILE SYSTEM</div>

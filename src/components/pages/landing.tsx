c3760b9f22b74114a4cfa4b7e912334629ad6cbc
snigdhakansal <snigdha0206@g.ucla.edu>
2022-08-07 22:09:41 -0700

    fixes button issues

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -4,7 +4,11 @@ import img from '../../assets/landing.svg';
 
 function Landing(): JSX.Element {
   return (
-    <div>
+    <div style={{ position: 'relative' }}>
+      <img src={img}></img>
+      <a href="intro">
+        <button className="startbutton">START</button>
+      </a>
       <Snowfall
         color="white"
         snowflakeCount={200}
@@ -12,10 +16,6 @@ function Landing(): JSX.Element {
         speed={[1.0, 5.0]}
         wind={[-0.5, 2.5]}
       />
-      <img style={{ width: '100vw' }} src={img}></img>
-      <a href="intro">
-        <button className="startbutton">START</button>
-      </a>
     </div>
   );
 }

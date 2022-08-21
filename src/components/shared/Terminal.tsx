a13818268946a5422a73278c0914411b49b49393
snigdhakansal <snigdha0206@g.ucla.edu>
2022-08-20 20:21:43 -0700

    draft

--- src/components/shared/Terminal.tsx
+++ src/components/shared/Terminal.tsx
@@ -1,6 +1,37 @@
 import React, { useState } from 'react';
 import '../../styles/Terminal.scss';
 
+function isValid(usertyped : string)
+{
+  switch(usertyped) {
+
+    case '$ whoami':
+      console.log('User input ls');
+      break;
+    case '$ pwd':
+      console.log('User input ls');
+      break;
+    case '$ man pwd':
+      console.log('User input ls');
+      break;
+    case '$ man whoami':
+      console.log('User input ls');
+      break;
+    case '$ ls':
+      console.log('User input ls');
+      break;
+    case '$ ls -a':
+      console.log('User input ls');
+      break;
+    case '$ ls -l':
+      console.log('User input ls');
+      break;
+    default:
+      console.log('Invalid command. Try again.');
+      break;
+  }
+}
+
 function Terminal(): JSX.Element {
   const [commands, setCommands] = useState<string[]>([]);
   const [input, setInput] = useState('$ ');
@@ -19,6 +50,7 @@ function Terminal(): JSX.Element {
     <div className="terminal">
       <div>
         {commands.map((command: string, key: number) => {
+          isValid(command);
           return (
             <p className="command" key={key}>
               {command}

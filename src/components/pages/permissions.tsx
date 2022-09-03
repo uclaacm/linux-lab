bee9d99b3c98b0536e226458784494b2cf1e0777
Lily Zhou <lzhou0714@g.ucla.edu>
2022-09-02 23:07:50 -0700

    linter

--- src/components/pages/permissions.tsx
+++ src/components/pages/permissions.tsx
@@ -4,76 +4,76 @@ import Task from './../shared/Task';
 
 function Permissions(): JSX.Element {
   const taskPrompts = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
     sed do eiusmod tempor incididunt ut labore et dolore magna\
      aliqua. Ut enim ad minim veniam, quis nostrud exercitation\
-     ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+     ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   ];
   return (
     <div>
-      <div className='permissionContainer'>
-      <h2 className="lesson-title">Permissions</h2>
-        <div className='body'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-           ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit
-             esse cillum dolore eu fugiat nulla pariatur.
+      <div className="permissionContainer">
+        <h2 className="lesson-title">Permissions</h2>
+        <div className="body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
         </div>
-        <div className='heading-1'>
+        <div className="heading-1">
           The first letter (red) indicates the file type.
         </div>
-        <div className='body'>
-        Lorem <span className='try-out-command'>d</span> dolor sit amet, 
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-        ut labore et dolore magna 
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        <div className="body">
+          Lorem <span className="try-out-command">d</span> dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation
         </div>
-        <div className='heading-1'>
+        <div className="heading-1">
           The first letter (red) indicates the file type.
         </div>
-        <div className='body'>
-        Lorem <span className='try-out-command'>d</span> dolor sit amet, 
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-        ut labore et dolore magna 
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        <div className="body">
+          Lorem <span className="try-out-command">d</span> dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation
         </div>
-        <div className='heading-1'>
+        <div className="heading-1">
           The first letter (red) indicates the file type.
         </div>
-        <div className='body'>
-        Lorem <span className='try-out-command'>d</span> dolor sit amet, 
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-        ut labore et dolore magna 
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        <div className="body">
+          Lorem <span className="try-out-command">d</span> dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation
         </div>
-        <div className='heading-1'>
+        <div className="heading-1">
           The first letter (red) indicates the file type.
         </div>
-        <div className='body'>
-        Lorem <span className='try-out-command'>d</span> dolor sit amet, 
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-        ut labore et dolore magna 
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        <div className="body">
+          Lorem <span className="try-out-command">d</span> dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation
         </div>
-        <h2 className='heading-1'>Subsection</h2>
-        <p className='body'>
-          Lorem    ipsum   dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna 
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-          </p>
-          <p className='body'>
-          Lorem    <span className='try-out-command'>command</span>  
-          dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna 
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-          </p>
-          <Task
-            taskPrompt = {taskPrompts[0]}
-            taskName = {"Task 1"}
-            completed = {false}
-          />
+        <h2 className="heading-1">Subsection</h2>
+        <p className="body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </p>
+        <p className="body">
+          Lorem <span className="try-out-command">command</span>
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation
+        </p>
+        <Task
+          taskPrompt={taskPrompts[0]}
+          taskName={'Task 1'}
+          completed={false}
+        />
         <img
           src={tuxHoldingEgg}
           alt="tux raising his flippers while holding an egg"

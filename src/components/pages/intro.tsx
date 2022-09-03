62eda64adb90a396a6f2b96ad90c0b930483e8c1
Lily Zhou <lzhou0714@g.ucla.edu>
2022-09-02 21:38:02 -0700

    text added for intro page

--- src/components/pages/intro.tsx
+++ src/components/pages/intro.tsx
@@ -30,12 +30,26 @@ function Intro(): JSX.Element {
 
       <div className="heading-1">THE POSIX FILE SYSTEM</div>
       <p className="body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
+        POSIX (Portable Operating System Interface) standard is one of the
+        primary standards adopted by Linux. It was created as part of a set of
+        standards to allow applications to run across different UNIX based
+        operating systems, such as Linux.
       </p>
+      <p className="body">
+        The POSIX file system system is organized in a tree-like format,
+        branching out from the root directory and into descendent directories
+        containing files and other subdirectories. The POSIX file system also
+        defines a set of operations that can be performed on files and
+        directories, such as open, close, read, and write.
+      </p>
+      <p className="body">
+        The POSIX file system system is organized in a tree-like format,
+        branching out from the root directory and into descendent directories
+        containing files and other subdirectories. The POSIX file system also
+        defines a set of operations that can be performed on files and
+        directories, such as open, close, read, and write.
+      </p>
+
       <img
         className="image-box"
         src={posixFileSystem}
@@ -44,11 +58,12 @@ function Intro(): JSX.Element {
       />
       <div className="heading-1">TUX</div>
       <p className="body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
+        Tux the penguin is the official brand character of Linux. The creator of
+        Linux, Linus Torvalds, wanted their branch character to be a penguin as
+        he was rumored to have contracted &ldquo;penguinitis&rdquo; after being
+        bitten by a penguin! Tux&apos;s was designed by Larry Ewing and named by
+        James Hughes. His name is meant to stand for &ldquo;(T)orvalds
+        (U)ni(X)&rdquo;, or tuxedo, which a penguin resembles
       </p>
     </div>
   );

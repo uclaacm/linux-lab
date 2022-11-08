d33594d80bdc3257042c5bd4e5da06b2f2a5f5d5
Juliet Zhang <julietzhang3@gmail.com>
2022-11-07 18:12:51 -0800

    Improve Landing page responsiveness * Separate graphics into individual components to improve responsiveness

--- src/components/pages/landing.tsx
+++ src/components/pages/landing.tsx
@@ -1,22 +1,38 @@
 import '../../styles/startButton.scss';
 import '../../styles/intro.scss';
 import Snowfall from 'react-snowfall';
-import img from '../../assets/landing.svg';
+import igloo from '../../assets/images/landing-igloo.svg';
+import leftIceberg from '../../assets/images/landing-left-iceberg.svg';
+import tux from '../../assets/images/landing-tux.svg';
+import waterShading from '../../assets/images/landing-water-shading.svg';
 
 function Landing(): JSX.Element {
   return (
     <div className="landing">
-      <img className="landing-background" src={img}></img>
-      <a href="intro">
-        <button className="startbutton">START</button>
-      </a>
-      <Snowfall
-        color="white"
-        snowflakeCount={200}
-        radius={[1.0, 6.0]}
-        speed={[1.0, 5.0]}
-        wind={[-0.5, 2.5]}
-      />
+      <div className="landing-background">
+        <img className="igloo" src={igloo}></img>
+        <img className="left-iceberg" src={leftIceberg}></img>
+        <img className="tux" src={tux}></img>
+        <img className="water-shading" src={waterShading}></img>
+        <p className="title">TUX&apos;S GREAT ADVENTURE</p>
+        <p className="iceberg-text">
+          Join Tux on an adventure
+          <br />
+          through Antarctica and
+          <br />
+          learn the basics of Linux!
+        </p>
+        <a href="intro">
+          <button className="startbutton">START</button>
+        </a>
+        <Snowfall
+          color="white"
+          snowflakeCount={200}
+          radius={[1.0, 6.0]}
+          speed={[1.0, 5.0]}
+          wind={[-0.5, 2.5]}
+        />
+      </div>
     </div>
   );
 }

c395d4511450c084617cab1652a89263e6aac4b5
jason2020 <jtay20@punahou.edu>
2022-12-27 17:50:23 -1000

    Removed dedent for now

--- src/components/shared/ManPages.ts
+++ src/components/shared/ManPages.ts
@@ -1,17 +1,15 @@
-import dedent from 'dedent';
-
 interface ManPage {
   [key: string]: string;
 }
 
 export const manPages: ManPage = {
-  whoami: dedent`
+  whoami: `
         whoami – display effective user id
           (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  pwd: dedent`
+  pwd: `
         pwd – return working directory name
           (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  ls: dedent`
+  ls: `
     ls – list directory contents.
       OPTIONS: 
         -a      Include directory entries whose names begin with a dot (‘.’).
@@ -20,31 +18,31 @@ export const manPages: ManPage = {
         modified, hour file last modified, minute file last modified, and the
         pathname.
         (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  cd: dedent`
+  cd: `
     cd — change the working directory
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  touch: dedent`
+  touch: `
     touch – change file access and modification times
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  mkdir: dedent`
+  mkdir: `
     mkdir – make directories
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  rmdir: dedent`
+  rmdir: `
     rmdir – remove directories
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  cp: dedent`
+  cp: `
     cp – copy files
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  mv: dedent`
+  mv: `
     mv – move files
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  echo: dedent`
+  echo: `
     echo – write arguments to the standard output
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  cat: dedent`
+  cat: `
     cat – concatenate and print files
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  grep: dedent`
+  grep: `
     grep - file pattern searcher. The grep utility searches any given input files, selecting lines that
       match one or more patterns.  By default, a pattern matches an input line
       if the regular expression (RE) in the pattern matches the input line
@@ -52,14 +50,14 @@ export const manPages: ManPage = {
       Each input line that matches at least one of the patterns is written to
       the standard output.
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  find: dedent`
+  find: `
     find – walk a file hierarchy. The find utility recursively descends the directory tree for each path
       listed, evaluating an expression in terms of each file in the tree.
       (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  chmod: dedent`
+  chmod: `
     chmod – change file modes or Access Control Lists.
     (DESCRIPTION SIMPLIFIED FOR LAB)`,
-  clear: dedent`
+  clear: `
     clear - clear the terminal screen
     (DESCRIPTION SIMPLIFIED FOR LAB)`,
 };
